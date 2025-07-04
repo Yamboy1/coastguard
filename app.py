442bcd5493aaa72e788c6138270aaf0c9ace79d9
Levi Lesches <levilesches@gmail.com>
2025-07-04 13:55:03 -0400

    Get username from MLN based on cookie.
    Follows an OAuth-like setup, where:
    - the server and MLN share a secret key
      - this secret key is never shared with the client
      - the key is stored in mln_secret.py, which is not checked into Git
    - the server sends the client a link to use to sign into MLN
    - after the signs into MLN, MLN redirects them to the server's endpoint
      with the user's original Coast Guard cookie and MLN username
    - the server associates the two and redirects back to the game
    - the client can now ask the server for the user's MLN username

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
+
 from lego_crypt import Crypt
+import mln
 
 app = Flask(__name__)
 
-body = """
+def generate_body(session_id, username): return f"""
 <root>
-    <username>Blah Blah Blah</username>
+    <username>{'' if username is None else username}</username>
     <token>Test</token>
     <data>
         <mylegobadge>1</mylegobadge>
@@ -16,18 +18,22 @@ body = """
         <totalscore>0</totalscore>
         <currentrank>1</currentrank>
         <levelsunlocked>0</levelsunlocked>
-        <loginurl></loginurl>
+        <loginurl>{mln.get_login_url(session_id)}</loginurl>
         <legowebsiteurl></legowebsiteurl>
-        <getbadgeurl></getbadgeurl>
+        <getbadgeurl>{mln.MLN_MAILBOX_URL}</getbadgeurl>
     </data>
 </root>
 """
 
-encrypted_body = Crypt.f_encrypt(body, Crypt.S_ENCRYPTION_KEY1)
+def encrypt_body(body):
+    return Crypt.f_encrypt(body, Crypt.S_ENCRYPTION_KEY1)
 
 @app.route("/InfoRequest.xml", methods=["POST"])
 def info_request():
-    print(Crypt.f_decrypt(request.form["_Body"]))
+    session_id = request.cookies.get("sessionid")
+    username = mln.SESSION_TO_USERNAME.get(session_id)
+    body = generate_body(session_id, username)
+    encrypted_body = encrypt_body(body)
     return (f'''
 <?xml version="1.0" encoding="utf-8"?>
 <string xmlns="http://www.lego.com/Services.HiScore/service">{encrypted_body}</string>
@@ -54,3 +60,8 @@ def legocoastguards():
 @app.route("/loader.swf")
 def loader():
     return app.send_static_file("loader.swf")
+
+@app.route("/api/login")
+def on_mln_login():
+    mln.on_login(**request.args)
+    return redirect("/")
---
+++ mln.py
@@ -0,0 +1,26 @@
+import requests
+from urllib.parse import urlencode
+
+from mln_secret import MLN_SECRET  # not tracked in git
+
+COAST_GUARD_URL = "http://localhost:5000"
+MLN_BASE_URL = "https://mln.lcdruniverse.org"
+# MLN_BASE_URL = "http://localhost:8000"
+MLN_MAILBOX_URL = f"{MLN_BASE_URL}/mln/private_view/default"
+MLN_API_LOGIN = "/api/coastguard/login"  # see mln-docs/mln.md
+
+SESSION_TO_USERNAME = {}
+
+def get_login_url(session_id):
+  # NOTE: The session_id goes into the redirect url, NOT the normal query paramters
+  # This is because MLN doesn't care about our session_id, but we want MLN to pass
+  # it back to us untouched so we can use that to know who signed in.
+  post_login_url = f"{COAST_GUARD_URL}/api/login?session_id={session_id}"
+  query_string = urlencode({
+    "respond_with_username": "yes",
+    "next": post_login_url,
+  })
+  return f"{MLN_BASE_URL}/accounts/login/?{query_string}"
+
+def on_login(session_id, username):
+  SESSION_TO_USERNAME[session_id] = username
